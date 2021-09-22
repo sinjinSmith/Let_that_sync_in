f0e215562b6fe3bd6693ebd8b56c3aec7b92ce9d
Sinjin <sinjinsmith15@gmail.com>
2021-09-22 00:26:30 -0700

    .01 test

--- main.py
+++ main.py
@@ -4,3 +4,11 @@ import shutil
 os.rename("path/to/current/file.foo", "path/to/new/destination/for/file.foo")
 os.replace("path/to/current/file.foo", "path/to/new/destination/for/file.foo")
 shutil.move("path/to/current/file.foo", "path/to/new/destination/for/file.foo")
+
+
+def start():
+    print('start')
+
+
+if "__init__" == '__main__':
+    start()
