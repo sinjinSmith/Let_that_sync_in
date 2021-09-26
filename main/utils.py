56b741dfaccc24f0226941047ac1a4aa3951d4cc
Sinjin <sinjinsmith15@gmail.com>
2021-09-26 00:55:41 -0700

    added subprocess and tkinter packages

--- main/utils.py
+++ main/utils.py
@@ -1,14 +1,33 @@
 import os
 import shutil
 import random
+from tkinter import filedialog
+import tkinter
+import os
+import subprocess
+FILEBROWSER_PATH = os.path.join(os.getenv('WINDIR'), 'explorer.exe')
 
+
+def explore(path):
+    # explorer would choke on forward slashes
+    path = os.path.normpath(path)
+
+    if os.path.isdir(path):
+        subprocess.run([FILEBROWSER_PATH, path])
+    elif os.path.isfile(path):
+        subprocess.run([FILEBROWSER_PATH, '/select,', os.path.normpath(path)])
+
+
+#explore("C:/Users/Sinjin/Desktop/Pythonic Works/!Active Projects/Let That Sync In")
+
+#tkinter.Tk().withdraw()  # prevents an empty tkinter window from appearing
+
+#folder_path = filedialog.askdirectory()
 
 
 #os.rename("path/to/current/file.foo", "path/to/new/destination/for/file.foo")
 #os.replace("path/to/current/file.foo", "path/to/new/destination/for/file.foo")
 #shutil.move("path/to/current/file.foo", "path/to/new/destination/for/file.foo")
-
-
 
 
 #os.mkdir(path, mode=0o777, *, dir_fd=None)
@@ -32,10 +51,6 @@ def create_txtFile(backup_path, fileName):
     create_txt.write("Testing content...")
     create_txt.close()
     return filePath
-
-
-
-
 
 
 
